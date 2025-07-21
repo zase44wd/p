25fa6a8708b031bfe95c28bfa5927019b16ac3c0
zase44wd <xzs66943@gmail.com>
2025-07-22 01:19:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/zase44wd/df")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/zase44wd/p")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN")
 CH_US = getenv("CH_US", "XXBTL")
